d4a5205308bbbbd854d2535f2802168416132265
yogirajgatkal <153065975+yogirajgatkal@users.noreply.github.com>
2023-12-07 21:49:48 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,8 @@
+const gamenumber=25;
+let usernumber=prompt("enter your number");
+
+while(usernumber != gamenumber)
+{
+    usernumber=prompt("enter your number");
+}
+console.log("congratulation you enter the right number");
